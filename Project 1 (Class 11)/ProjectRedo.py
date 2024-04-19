e5e090557df0c16eca99d456298fed85ae7fd83f
Sarah Johnston <johnstons91@outlook.com>
2024-04-19 15:51:30 -0400

    update project redo coding

--- Project 1 (Class 11)/ProjectRedo.py
+++ Project 1 (Class 11)/ProjectRedo.py
@@ -1,6 +1,7 @@
 import re
 
 Usernames_Taken = ['admin', 'admin123', 'superuser', 'superuser123']
+
 #Meeting Requirements to create username:
 def does_username_meet_requirements(username):
     if not username[0].islower() or not username.isidentifier():
@@ -19,8 +20,43 @@ def does_password_meet_requirements(password):
 
 
 #Sign-up to Get user ID and Password:
-username = input("\nCreate username to login:")
 
+def sign_up():
+    print("Please sign up by creating a Username and Password to continue.")
+#Prompt to create username:
 while True:
+    username = input("\nCreate username to login. \nUsername reqirements: \n ~Username must start with a lowercase letter and only contain letters, numbers and underscores.")
+    if does_username_meet_requirements(username):
+        print("\nYour username has been created. Lets now create a password")
+        break
+    else:
+        print("Invaild Username. Username requirements: \n ~Username must start with a lowercase letter and only contain letters, numbers and underscores.")
+    if username in Usernames_Taken:
+        print("Username is taken. Please create a different username.")
 
+    #Prompt to create passaword:
+    print("\nPassword Requirements: \nPassword must be at least 8 characters long. \n~Contain at least one uppercase letter. \n~Contain at least one lowercase letter. \n~Contain at least one digit. \n~Contain at least one of these characters (!, ?, @, #, $, ^, &, *, _, -. \nDoes not contain any spaces. ")
+while True:
+    password = input("Create a password to complete the sign-up process: ")
+    if does_password_meet_requirements(password):
+        break
+    else:
+        print("The password is invalid. Please try again. \nPassword Requirements: \nPassword must be at least 8 characters long. \n~Contain at least one uppercase letter. \n~Contain at least one lowercase letter. \n~Contain at least one digit. \n~Contain at least one of these characters (!, ?, @, #, $, ^, &, *, _, -. \nDoes not contain any spaces.")
+print("\nYou have successfully signed-up. please continue to log-in")
 
+log_in(username, password)
+        
+#Verify username log-in after successful sign-up.
+def log_in(username, password):
+    print("\nPlease log-in to your account:")
+while True:
+    username_input = input("Enter your username: ")
+    if username_input == username:
+        break 
+    else:
+        print("Username is incorrect. Please attempt to try again.")
+#Verify username log-in after successful sign-up:
+while True:
+    password_input = input("Enter your password: ")
+    if password_input == password:
+        print("Log-in successful! Thank you for joining. ")
