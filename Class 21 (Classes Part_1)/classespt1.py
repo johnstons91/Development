491abfaa8a88426d5a2a9d8e7e7311d918c494bd
Sarah Johnston <johnstons91@outlook.com>
2024-05-07 11:29:44 -0400

    classes pt1 notes

--- Class 21 (Classes Part_1)/classespt1.py
+++ Class 21 (Classes Part_1)/classespt1.py
@@ -9,18 +9,18 @@ Classes
 
 
 
-class Point2d:
-     '''Initializer -> Has to happen in every class'''
-     def __init__(self, x=0, y=0):
-          self.x = x
-          self.y = y  
-     '''String Representation (PROJECT) '''   
-     def __str__(self):
-          return f'({self.x}), ({self.y})'
+# class Point2d:
+#      '''Initializer -> Has to happen in every class'''
+#      def __init__(self, x=0, y=0):
+#           self.x = x
+#           self.y = y  
+#      '''String Representation (PROJECT) '''   
+#      def __str__(self):
+#           return f'({self.x}), ({self.y})'
      
-#Creating an object of the Point2D Class
-point1 = Point2d(4,10)
-point2 = Point2d(8,15)
+# #Creating an object of the Point2D Class
+# point1 = Point2d(4,10)
+# point2 = Point2d(8,15)
 
     #  #Add your object to another object of the same class
     #  def __add__(self, other):
@@ -150,12 +150,67 @@ Without the __eq__ method, we will only be able to test if it is the same object
 
 
 
-''' Exercise - Date Class '''
+''' Exercise - Date Class 
+
+1. Display the date in a format of mm/dd/yyyy
+2. Compare 2 different dates, if they are equal,
+3. Compare which date came first.
+4. Determine if a date is a leap year
 
 
+Functions need to be behind the def
+'''
+
+class Date:
+        
+        def __init__(self, year=1970, month=1, day=1):
+                '''These are our parameters'''
+                self.year = year
+                self.month = month
+                self.day = day
+
+        #this will control what the print built in function displays
+        def __str__(self):
+                return f'Month: {self.month:02d}\nDay:{self.day:02d}\nYear: {self.year:02d}'
+        
+        #this will control what == does in your class
+        def __eq__(self, other):
+                if self.year == other.year and self.month == other.month and self.day == other.day:
+                        return True
+                return False
+        
+        #create a method to handle less than date objects, which date came first?
+        def __lt__(self, other):
+                selfdate = datetime.datetime(self.year, self.month, self.day)
+                otherdate = datetime.datetime(other.year, other.month, other.day)
+                if selfdate < otherdate:
+                        return True
+                return False
+        
+        def is_leap_year(self, other):
+               
+                if (self.year % 4 == 0 and self.year % 100 != 0 ) or ( self.year % 400 == 0): 
+                        return True 
+                else :
+                        return False
 
 
- 
+my_date_info = Date(2004, 10, 4)  #Create the object
+second_date = Date(2004, 10, 4)
+print('Today\'s date is', my_date_info)
+
+#Equality
+print(my_date_info == second_date)
+
+#Less Than
+old_date = Date(1998, 2, 10)
+new_date = Date(2000, 2, 10)
+
+print(old_date < new_date)
+
+#Leap Year
+my_new_date = Date(2009, 6, 1)
+print(my_new_date.is_leap_year()) 
 
 
 
